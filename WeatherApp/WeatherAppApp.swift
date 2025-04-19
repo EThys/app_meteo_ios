8264d79bb0d9702b5811b523a3353d555794c56e
Ethys <ethbergmuzola500@gmail.com>
2025-04-19 13:12:55 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Muzola Ethberg on 19/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
